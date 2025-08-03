56503da16cd4158826dea9af316763a3a9c64a1a
holawaleh <holawaleh@gmail.com>
2025-08-03 12:55:32 +0100

    student

--- models/Student.js
+++ models/Student.js
@@ -2,19 +2,48 @@ const mongoose = require("mongoose");
 
 const studentSchema = new mongoose.Schema(
   {
-    name: String,
-    matricNo: String,
-    email: String,
-    level: String,
-    phone: String,
-    department: String,
-    uid: String,
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    matricNo: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    level: {
+      type: String,
+      trim: true,
+    },
+    phone: {
+      type: String,
+      trim: true,
+    },
+    department: {
+      type: String,
+      trim: true,
+    },
+    uid: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
     subjects: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Subject"
-      }
-    ]
+        ref: "Subject",
+      },
+    ],
   },
   { timestamps: true }
 );
