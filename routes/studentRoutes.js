698357cf405ac980b09507b8a5ddcffca30c382d
holawaleh <holawaleh@gmail.com>
2025-11-04 07:29:06 +0100

    commit

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require('mongoose');
 const Student = require("../models/Student");
 const Log = require("../models/Log"); // ⬅️ Import Log model
 
